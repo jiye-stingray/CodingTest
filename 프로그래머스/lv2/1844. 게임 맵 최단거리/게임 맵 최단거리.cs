8bec240ca93392667ce3ac078cef8d2dc3bf9e9f
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-08-30 15:33:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 1.75 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class Solution
+    {
+
+
+        public int solution(int[,] maps)
+        {
+            int h;
+            int v;
+
+            int answer = 0;
+            h = maps.GetLength(0);
+            v = maps.GetLength(1);
+
+            bool isEnd = false; 
+            
+            Queue<Tuple<int,int,int>> _queue = new Queue<Tuple<int, int, int>>();
+
+
+
+            _queue.Enqueue(new Tuple<int, int, int>(0, 0, 1));
+            
+            // 방문 표시를 위한 맵 2차원 배열 
+            bool[,] bools = new bool[h,v];
+
+
+            // 델타 값
+            int[] dx = new int[] { -1, 0, 1, 0 };
+            int[] dy = new int[] { 0, 1, 0, -1 };
+
+            while(_queue.Count > 0)
+            {
+                var current = _queue.Dequeue();     // 현재 위치
+                int x = current.Item1;
+                int y = current.Item2;
+                int dis = current.Item3;
+
+                if(x == h - 1 && y == v - 1)
+                {
+                    isEnd= true;
+                    answer = dis;
+                    return answer;
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int newX = x + dx[i];
+                    int newY = y + dy[i];
+
+
+                    if(newX >= 0 && newY >= 0 && newX < h && newY < v && !bools[newX,newY] && maps[newX,newY] == 1 )
+                    {
+                        _queue.Enqueue(new Tuple<int, int, int>(newX, newY, dis + 1));
+                        bools[newX,newY] = true;
+                        answer = dis + 1;
+                    }
+                    
+                }
+            }
+
+            if (!isEnd) return -1;
+            
+            return answer;
+        }
+    }
