81ab8c4edd3664c1903e5880064036d966249cb9
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-11-03 18:41:43 +0900

    [level unrated] Title: 배열 조각하기, Time: 1.38 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = new int[] {};
+
+        for (int i = 0; i < query.Length; i++)
+        {
+            
+            if(i%2 == 0)
+            {
+                arr = arr.Take(query[i] + 1).ToArray();
+            }
+            else
+            {
+                arr = arr.Skip(query[i]).ToArray();
+            }
+        }
+
+        answer = arr;
+
+        return answer;
+    }
+}
